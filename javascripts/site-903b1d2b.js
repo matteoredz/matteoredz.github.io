aba4b24a5498b05529c40bf13c8fc49d401429a2
matteoredz <matteoredz@users.noreply.github.com>
2020-12-12 20:14:32 +0000

    deploy: 8e5bfe05c0c673ee8bd6c95b72106115db96fa3a

--- javascripts/site-903b1d2b.js
+++
@@ -1,6 +0,0 @@
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-import 'codyhouse-framework/main/assets/js/util.js'
-import './components/_1_header.js'
